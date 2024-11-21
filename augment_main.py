1901ca5de992afa14c2b9a4d5a92498177810276
maany <imptodefeat@gmail.com>
2024-11-21 23:27:47 +0100

    v1 release prep

--- augment_main.py
+++ augment_main.py
@@ -10,7 +10,6 @@ def main(
     case_study_name: str,
     job_id: int,
     tracer_id: str,
-    work_dir: str,
     kp_auth_token: str,
     kp_host: str,
     kp_port: int,
@@ -19,12 +18,11 @@ def main(
 ) -> None:
 
     logger = logging.getLogger(__name__)
-    logging.basicConfig(level=log_level)
-
+    logging.basicConfig(level=log_level, format='%(asctime)s - %(levelname)s - %(message)s')
     try:
     
         if not all([case_study_name, job_id, tracer_id]): 
-            logger.error(f"{job_id}: job_id, tracer_id must all be set.") 
+            logger.error(f"job_id, tracer_id must all be set.") 
             raise ValueError("job_id, tracer_id must all be set.")
 
         kernel_planckster, protocol, file_repository = setup(
@@ -42,22 +40,30 @@ def main(
             file_repository=file_repository,
         )
 
+        root_relative_path = f"{case_study_name}/{tracer_id}/{job_id}"
+        relevant_files = kernel_planckster.list_source_data(root_relative_path)
+        if not relevant_files or len(relevant_files) == 0:
+                logger.error(f"No relevant files found in {root_relative_path}.")
+                sys.exit(1)
+        print(relevant_files)
     except Exception as e:
         logger.error(f"Error setting up scraper: {e}")
         sys.exit(1)
-
-
-    augment(
+    
+    job_output = augment(
         case_study_name=case_study_name,
         job_id=job_id,
         tracer_id=tracer_id,
         scraped_data_repository=scraped_data_repository,
+        relevant_source_data=relevant_files,
         log_level=log_level,
-        work_dir = work_dir,
         protocol=protocol,
     )
 
+    logger.info(f"{job_id}: Scraper finished with state: {job_output.job_state.value}")
 
+    if job_output.job_state.value == "failed":
+        sys.exit(1)
 
 if __name__ == "__main__":
 
@@ -91,14 +97,6 @@ if __name__ == "__main__":
         type=str,
         default="WARNING",
         help="The log level to use when running the pipeline. Possible values are DEBUG, INFO, WARNING, ERROR, CRITICAL. Set to WARNING by default.",
-    )
-
-
-    parser.add_argument(
-        "--work_dir",
-        type=str,
-        default="./.tmp",
-        help="work dir",
     )
 
     parser.add_argument(
@@ -137,7 +135,6 @@ if __name__ == "__main__":
         job_id=args.job_id,
         tracer_id=args.tracer_id,
         log_level=args.log_level,
-        work_dir = args.work_dir,
         kp_auth_token=args.kp_auth_token,
         kp_host=args.kp_host,
         kp_port=args.kp_port,
